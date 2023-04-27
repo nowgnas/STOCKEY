7e19035023166a7bc5e46ec9040e0a4aff7db80d
nowgnas <dev.nowgnas@gmail.com>
2023-04-28 08:44:39 +0900

    :tada: feat: create gateway service

---
+++ server/gateway-service/src/test/java/kr/stockey/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.stockey.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

dddafedadf2bbfe692d4c4cd7ddcdcbeaba7a342
junmo <devjunmo@gmail.com>
2023-05-02 18:04:57 +0900

    🎉 feat: investment 프로젝트 생성

---
+++ server/investment-service/src/main/java/kr/stockey/investmentservice/InvestmentServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.stockey.investmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentServiceApplication.class, args);
+    }
+
+}

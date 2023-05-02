757e5f7b91e102624f1ca256f62a2a03336c8680
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-05-02 13:26:49 +0900

    ✅ feat: Add DndBox zoom animation

--- frontend/src/components/LabPage/DndBox.tsx
+++ frontend/src/components/LabPage/DndBox.tsx
@@ -1,4 +1,4 @@
-import { memo, useState } from "react";
+import { memo, useState, useRef } from "react";
 import { useSetRecoilState } from "recoil";
 import {
   selectedLabStockState,
@@ -10,7 +10,8 @@ import {
 import StockCardMini from "./StockCardMini";
 import KeywordCardMini from "./KeywordCardMini";
 
-import DeleteIcon from '@mui/icons-material/Delete';
+import Zoom from '@mui/material/Zoom';
+import DeleteIcon from "@mui/icons-material/Delete";
 import styled from "styled-components";
 
 interface Props {
@@ -24,59 +25,82 @@ const DndBox = ({ type, item }: Props) => {
   const setStock = useSetRecoilState(selectedLabStockState);
   const setKeywordList = useSetRecoilState(selectedLabKeywordListState);
   const [mouseOver, setMouseOver] = useState(false);
-
-  // item == undefined일 경우
-  if (!item) {
-    return <DropBox>{text}</DropBox>;
-  }
-
+  const animeRef = useRef(true)
+  
   const deleteHandler = () => {
     console.log("deleteHandler!");
-    if (type === "STOCK") {
-      setStock(undefined);
-    } else {
-      const idx = parseInt(type[7]) - 1;
-      setKeywordList((prev) => prev.filter((item, index) => index !== idx));
-    }
+    animeRef.current = false;
+    // zoom out animation 위해 settimeout
+    setTimeout(() => {
+      if (type === "STOCK") {
+        setStock(undefined);
+      } else {
+        const idx = parseInt(type[7]) - 1;
+        setKeywordList((prev) => prev.filter((item, index) => index !== idx));
+      }
+      animeRef.current = true;
+    }, 200);
   };
 
   return (
-    <CardWrapper
-      onMouseEnter={() => setMouseOver(true)}
-      onMouseLeave={() => setMouseOver(false)}
-    >
-      {type === "STOCK" ? (
-        <StockCardMini item={item} />
+    <BoxWrapper>
+      {(!item) ? (
+        <DropBox>
+          <DropImg src={'labImages/dragDropLogo.png'} />
+          {text}
+        </DropBox>
       ) : (
-        <KeywordCardMini item={item} />
+        <Zoom in={animeRef.current}>
+          <CardWrapper
+            onMouseEnter={() => setMouseOver(true)}
+            onMouseLeave={() => setMouseOver(false)}
+          >
+            {type === "STOCK" ? (
+              <StockCardMini item={item} />
+            ) : (
+              <KeywordCardMini item={item} />
+            )}
+            {mouseOver && (
+              <IconWrapper onClick={deleteHandler}>
+                <DeleteIcon sx={{ fontSize: "5rem" }} />
+              </IconWrapper>
+            )}
+          </CardWrapper>
+        </Zoom>
       )}
-      {mouseOver && (
-        <IconWrapper onClick={deleteHandler}>
-          <DeleteIcon sx={{fontSize: "5rem"}}/>
-        </IconWrapper>
-      )}
-    </CardWrapper>
+    </BoxWrapper>
   );
 };
 
 export default memo(DndBox);
 
-const DropBox = styled.div`
+const BoxWrapper = styled.div`
   width: 120px;
   height: 120px;
+  background: #f8f8f8;
+  border-radius: 24px;
+  border: 1px dashed #cac4d0;
 
   display: flex;
   justify-content: center;
   align-items: center;
+`
 
+const DropBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
   font-size: 12px;
   font-weight: 700;
   color: #79747e;
-  background: #f8f8f8;
-
-  border: 1.5px dashed #cac4d0;
-  border-radius: 24px;
 `;
+
+const DropImg = styled.img`
+`
 
 const CardWrapper = styled.div`
   width: 120px;
@@ -94,10 +118,10 @@ const IconWrapper = styled.div`
   align-items: center;
   top: 0%;
   right: 0%;
-  
+
   border-radius: 24px;
-  border: 1px solid #BFBFBF;
-  color: #FB6F6F;
+  border: 1px solid #bfbfbf;
+  color: #fb6f6f;
   font-weight: bold;
   background-color: rgba(0, 0, 0, 0.2);
 `;

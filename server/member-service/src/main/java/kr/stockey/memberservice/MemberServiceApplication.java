994200affbf414cda7956e7306336e270fa56a28
공진호 <wlsgh7608@naver.com>
2023-05-04 13:32:11 +0900

    🎉 feat: member 프로젝트 생성

---
+++ server/member-service/src/main/java/kr/stockey/memberservice/MemberServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.stockey.memberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberServiceApplication.class, args);
+    }
+
+}

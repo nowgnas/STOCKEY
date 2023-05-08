0f418bf134c1be96a56d628ecb2731b1a2b847c5
nowgnas <dev.nowgnas@gmail.com>
2023-05-08 19:42:43 +0900

    :wrench: update: amend file name

--- server/apigateway-service/src/main/java/kr/stockey/apigatewayservice/ApigatewayServiceApplication.java
+++ server/apigateway-service/src/main/java/kr/stockey/apigatewayservice/ApigatewayServiceApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ApigatewayServiceApplication {
 
     public static void main(String[] args) {

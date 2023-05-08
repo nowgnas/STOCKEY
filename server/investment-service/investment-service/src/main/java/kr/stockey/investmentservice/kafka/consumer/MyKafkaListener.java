b8acf3f1f53155c3049a1cd956a5d79df05cd5c5
junmo <devjunmo@gmail.com>
2023-05-08 13:02:50 +0900

    🚑️ feat: consumer 리스너와 config 수정
    Former-commit-id: 68f67999e42fc946ad9d7d6cc36344e448d74071

--- server/investment-service/investment-service/src/main/java/kr/stockey/investmentservice/kafka/consumer/MyKafkaListener.java
+++ server/investment-service/investment-service/src/main/java/kr/stockey/investmentservice/kafka/consumer/MyKafkaListener.java
@@ -1,21 +1,35 @@
 package kr.stockey.investmentservice.kafka.consumer;
 
 import kr.stockey.investmentservice.dto.OrderProducerDto;
+import kr.stockey.investmentservice.redis.Order;
+import kr.stockey.investmentservice.redis.OrderRedisRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@RequiredArgsConstructor
 public class MyKafkaListener {
-    @KafkaListener(topics = "stock-orders",groupId = "your-group-id4")
+
+//    private final OrderRedisRepository orderRedisRepository;
+//
+//
+//    public void saveOrderToRedis(Order order) {
+//        orderRedisRepository.save(order);
+//    }
+
+    @KafkaListener(topics = "stock-orders",groupId = "group-id-1", containerFactory = "kafkaListenerContainerFactory1")
     public void receive1(OrderProducerDto order) {
         // do something with the message
         System.out.println("order1 = " + order);
+        // 들어온 주문을 레디스에 저장
+
     }
 
-    @KafkaListener(topics = "order-test-topic", groupId = "your-group-id4")
-    public void receive2(OrderProducerDto order) {
-        // do something with the message
-        System.out.println("order2 = " + order);
-    }
+//    @KafkaListener(topics = "order-test-topic", groupId = "group-id-1")
+//    public void receive2(OrderProducerDto order) {
+//        // do something with the message
+//        System.out.println("order2 = " + order);
+//    }
 }

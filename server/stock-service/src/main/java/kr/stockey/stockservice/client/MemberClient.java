7db91da74449d4f25657f122d26276aaae1bda39
공진호 <wlsgh7608@naver.com>
2023-05-08 11:06:44 +0900

    🔧 amend : 파일명 변경

--- server/stock-service/src/main/java/kr/stockey/stockservice/client/MemberClient.java
+++ server/stock-service/src/main/java/kr/stockey/stockservice/client/MemberClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "member-service")
-public interface MemberServiceClient {
+public interface MemberClient {
     @GetMapping("/{userId}")
     ResponseDto getMemberResponse(@PathVariable("userId") String userId);
 }

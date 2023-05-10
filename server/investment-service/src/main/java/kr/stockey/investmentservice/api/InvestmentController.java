dc0484bca3294ed15b81cb7109cf521e80946ac8
junmo <devjunmo@gmail.com>
2023-05-10 16:49:59 +0900

    ✅ feat: responseDto 추가
    Former-commit-id: e3e870d84ec311441554f4ae0f3375838a236801

--- server/investment-service/src/main/java/kr/stockey/investmentservice/api/InvestmentController.java
+++ server/investment-service/src/main/java/kr/stockey/investmentservice/api/InvestmentController.java
@@ -3,14 +3,14 @@ package kr.stockey.investmentservice.api;
 import kr.stockey.investmentservice.api.request.OrderRequest;
 import kr.stockey.investmentservice.dto.OrderListDto;
 import kr.stockey.investmentservice.dto.OrderProducerDto;
+import kr.stockey.investmentservice.dto.ResponseDto;
 import kr.stockey.investmentservice.kafka.producer.StockOrderProducer;
 import kr.stockey.investmentservice.mapper.InvestmentDtoMapper;
 import kr.stockey.investmentservice.service.InvestmentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -24,11 +24,13 @@ public class InvestmentController {
     private final InvestmentDtoMapper investmentDtoMapper;
 
     @PostMapping("/order")
-    public void takeStockOrder(@RequestBody List<OrderRequest> orderRequests) throws Exception {
+    public ResponseEntity<ResponseDto> takeStockOrder(@RequestBody List<OrderRequest> orderRequests) throws Exception {
         List<OrderListDto> orderListDto = investmentDtoMapper.toOrderListDto(orderRequests);
         OrderProducerDto orderProducerDto = new OrderProducerDto(getMemberId(), orderListDto, LocalDateTime.now());
         investmentService.takeStockOrder(orderProducerDto);
+        return new ResponseEntity<>(new ResponseDto("주문 제출 완료!", null), HttpStatus.OK);
     }
+
 
     private Long getMemberId() {
         // http 헤더에서 "X-UserId" 내용 가져와서 리턴하는 로직으로 채우기

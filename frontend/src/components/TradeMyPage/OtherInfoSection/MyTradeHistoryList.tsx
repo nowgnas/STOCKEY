beb970546dae12848eb6923987ab4211d6b31a28
kaeng2 <6oston.ivy@gmail.com>
2023-05-10 15:29:58 +0900

    ✅ : MyCurrentTradeHistoryCard 퍼블리싱 완료. 추후 클릭 이벤트 추가 예정

--- frontend/src/components/TradeMyPage/OtherInfoSection/MyTradeHistoryList.tsx
+++ frontend/src/components/TradeMyPage/OtherInfoSection/MyTradeHistoryList.tsx
@@ -1,19 +1,22 @@
 import styled from "styled-components"
 import MyTradeHistoryCard from "./MyTradeHistoryCard"
+import MyCurrentTradeHistoryCard from "./MyCurrentTradeHistoryCard"
 import { PanelTitle } from "../../StockDetailPage/SubPanel/KeywordPanel/KeywordPanel"
 
 const MyTradeHistoryList = () => {
-  const DUMMY_DATA = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const DUMMY_DATA = [35000, -1600, 0, -105800, 4100, 700]
   return (
     <>
-      <PanelTitle style={{ margin: "0 0 16px 5px" }}>내 주문 기록</PanelTitle>
-      <div className="scroll-x">
+      <PanelTitle style={{ margin: "0 0 2px 5px" }}>내 주문 기록</PanelTitle>
+      <div className="scroll-x" style={{ overflowY: "visible" }}>
         <CardListContainer>
+          <MyCurrentTradeHistoryCard isSubmitted={true} />
+          <MyCurrentTradeHistoryCard isSubmitted={false} />
           {DUMMY_DATA.map((item) => (
             <MyTradeHistoryCard
               key={item}
               date="5월 9일 15시"
-              realizedProfit={-35600}
+              realizedProfit={item}
             />
           ))}
         </CardListContainer>
@@ -27,4 +30,5 @@ export default MyTradeHistoryList
 const CardListContainer = styled.div`
   // 레이아웃
   display: inline-flex;
+  padding: 3%;
 `

13ddb99d537bbf71aff52b8757ca0e75adf0d5cd
조현철 <chaoman5@naver.com>
2023-05-12 09:19:04 +0900

    ⚡️ : 불필요한 코드 제거, 공통 함수 하나 추가

--- frontend/src/components/TradeOrderPage/TradeForm/TradeConfirmModalItem.tsx
+++ frontend/src/components/TradeOrderPage/TradeForm/TradeConfirmModalItem.tsx
@@ -23,7 +23,7 @@ const TradeConfirmModalItem = ({
   moveCardHandler,
 }: Props) => {
   const ref = useRef<HTMLDivElement | null>(null)
-  const [{ isDragging }, dragRef, previewRef] = useDrag(
+  const [, dragRef, previewRef] = useDrag(
     () => ({
       type: itemInfo.status,
       item: itemInfo,
@@ -31,17 +31,12 @@ const TradeConfirmModalItem = ({
         //isDragging 변수가 현재 드래깅중인지 아닌지를 리턴
         isDragging: monitor.isDragging(),
       }),
-      end: (item, monitor) => {
-        // 드래그가 끝났을때
-        const dropResult: { dropEffect: string; name: string } | null =
-          monitor.getDropResult()
-      },
     }),
     [itemInfo]
   )
 
-  const [{ getItem, getItemType }, drop] = useDrop({
-    accept: ["MYSELL"],
+  const [, drop] = useDrop({
+    accept: ["MYBUY"],
     collect: (monitor) => ({
       getItem: monitor.getItem() as Props,
       getItemType: monitor.getItemType(),
@@ -54,36 +49,22 @@ const TradeConfirmModalItem = ({
       const dragIndex = item.index
       const hoverIndex = itemInfo.index
 
-      // Don't replace items with themselves
       if (dragIndex === hoverIndex) {
         return
       }
-      // Determine rectangle on screen
       const hoverBoundingRect = ref.current?.getBoundingClientRect()
-      // Get vertical middle
       const hoverMiddleY =
         (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
-      // Determine mouse position
       const clientOffset = monitor.getClientOffset()
-      // Get pixels to the top
       const hoverClientY = clientOffset!.y - hoverBoundingRect.top
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
-      // Dragging downwards
+
       if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
         return
       }
-      // Dragging upwards
       if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
         return
       }
-      // Time to actually perform the action
       moveCardHandler!(dragIndex, hoverIndex)
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
       //@ts-ignore
       item.index = hoverIndex
     },
@@ -96,7 +77,7 @@ const TradeConfirmModalItem = ({
 
   return (
     <TextWrapper
-      ref={itemInfo.status === "MYSELL" ? ref : null}
+      ref={itemInfo.status === "MYBUY" ? ref : null}
       opacity={opacity}
       container
       direction="row"

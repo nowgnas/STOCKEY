775082cda2bba98d2af68f795b357642dfd007fb
공진호 <wlsgh7608@naver.com>
2023-05-12 10:34:31 +0900

    🎉 feat: news 프로젝트 생성

---
+++ server/news-service/src/main/java/kr/stockey/newsservice/NewsServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.stockey.newsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsServiceApplication.class, args);
+    }
+
+}

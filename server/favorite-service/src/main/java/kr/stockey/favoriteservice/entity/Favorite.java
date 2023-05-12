75069895783542a8066b3830c08dbf8255806a53
공진호 <wlsgh7608@naver.com>
2023-05-12 16:04:50 +0900

    ✨ feat: Entity 변경

--- server/favorite-service/src/main/java/kr/stockey/favoriteservice/entity/Favorite.java
+++ server/favorite-service/src/main/java/kr/stockey/favoriteservice/entity/Favorite.java
@@ -1,11 +1,6 @@
 package kr.stockey.favoriteservice.entity;
 
-import com.ssafy.backend.domain.industry.entity.Industry;
-import com.ssafy.backend.domain.keyword.entity.Keyword;
-import com.ssafy.backend.domain.member.entity.Member;
-import com.ssafy.backend.domain.stock.entity.Stock;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,41 +18,18 @@ public class Favorite {
     private Long id;
 
     @NotNull
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "member_id", nullable = false)
-    private Member member;
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
 
-    @ManyToOne(fetch = FetchType.LAZY,optional = true)
-    @JoinColumn(name = "stock_id",nullable = true)
-    private Stock stock;
+    @Column(name = "stock_id",nullable = true)
+    private Long stockId;
 
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = true)
-    @JoinColumn(name = "industry_id", nullable = true)
-    private Industry industry;
+    @Column(name = "industry_id", nullable = true)
+    private Long industryId;
 
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = true)
-    @JoinColumn(name = "keyword_id", nullable = true)
-    private Keyword keyword;
-
-
-    @Builder(builderClassName = "industryBuilder", builderMethodName = "industryBuilder")
-    public Favorite(Member member,Industry industry){
-        this.member = member;
-        this.industry = industry;
-    }
-
-    @Builder(builderClassName = "stockBuilder", builderMethodName = "stockBuilder")
-    public Favorite(Member member,Stock stock){
-        this.member = member;
-        this.stock = stock;
-    }
-
-    @Builder(builderClassName = "keywordBuilder", builderMethodName = "keywordBuilder")
-    public Favorite(Member member,Keyword keyword){
-        this.member = member;
-        this.keyword = keyword;
-    }
+    @Column(name = "keyword_id", nullable = true)
+    private Long keywordId;
 
 }

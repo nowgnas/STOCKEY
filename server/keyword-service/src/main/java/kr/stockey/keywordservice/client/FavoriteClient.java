2e12c52f3b1fd788110dbdf7bf17930069f6a9db
공진호 <wlsgh7608@naver.com>
2023-05-13 16:30:17 +0900

    ✨ feat: favorite MS 메소드 정의

--- server/keyword-service/src/main/java/kr/stockey/keywordservice/client/FavoriteClient.java
+++ server/keyword-service/src/main/java/kr/stockey/keywordservice/client/FavoriteClient.java
@@ -1,7 +1,25 @@
 package kr.stockey.keywordservice.client;
 
+import kr.stockey.keywordservice.dto.core.ResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "favorite-service")
 public interface FavoriteClient {
+
+    @GetMapping("/keyword/my")
+    ResponseDto getMyFavoriteKeyword();
+
+    @GetMapping("keyword/check/{keywordId}")
+    ResponseDto checkFavoriteKeyword(@PathVariable Long keywordId);
+
+    @PostMapping("keyword/{keywordId}")
+    ResponseDto createFavoriteKeyword(@PathVariable Long keywordId);
+
+    @DeleteMapping("/{favoriteId}")
+    Void deleteFavorite(@PathVariable Long favoriteId);
+
 }

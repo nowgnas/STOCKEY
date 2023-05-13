9cead25dc13019a4de979296d735eac7b12a80e5
공진호 <wlsgh7608@naver.com>
2023-05-13 21:40:52 +0900

    🎉 feat: industry 프로젝트 생성

---
+++ server/industry-service/src/main/java/kr/stockey/industryservice/IndustryServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.stockey.industryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustryServiceApplication.class, args);
+	}
+
+}

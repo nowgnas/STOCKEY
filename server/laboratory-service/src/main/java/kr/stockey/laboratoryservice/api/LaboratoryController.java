819394d3c1828c78b52305a00cd3573f084348ea
nowgnas <dev.nowgnas@gmail.com>
2023-05-13 23:58:32 +0900

    :sparkles: Feat: add search keyword controller

--- server/laboratory-service/src/main/java/kr/stockey/laboratoryservice/api/LaboratoryController.java
+++ server/laboratory-service/src/main/java/kr/stockey/laboratoryservice/api/LaboratoryController.java
@@ -17,18 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class LaboratoryController {
     private final LaboratoryService laboratoryService;
 
-    @GetMapping("health")
-    public ResponseEntity<ResponseDto> healthCheck() {
-        return ResponseEntity.ok(
-                ResponseDto.builder()
-                .data("laboratory health check success")
-                .build());
-    }
-
     /**
      * 주식 이름으로 일치하는 주식 종목 찾기
      *
-     * @param name 사용자 입력 주식 이름
+     * @param stock 사용자 입력 주식 이름
      * @return 사용자가 입력한 이름으로 시작하는 주식 리스트 반환
      * TODO: - 리스트 페이징 처리가 필요한가
      *       - 입력단어를 시작으로 할 것인가
@@ -41,13 +33,27 @@ public class LaboratoryController {
                     @ApiResponse(responseCode = "400", description = "잘못된 요청")
             }
     )
-    @GetMapping("search/{name}")
-    public ResponseEntity<ResponseDto> searchStock(@PathVariable String name) {
-        SearchStockResponse searchStockResponse = laboratoryService.searchStock(name);
+    @GetMapping("stock/{stock}")
+    public ResponseEntity<ResponseDto> searchStock(@PathVariable String stock) {
+        SearchStockResponse searchStockResponse = laboratoryService.searchStock(stock);
 
         return ResponseEntity.ok(ResponseDto.builder()
                 .message("request success")
                 .data(searchStockResponse)
                 .build());
+    }
+
+    /**
+     * 키워드 검색
+     * @param keyword 사용자 입력 키워드 이름
+     * @return 사용자 입력 이름을 포함한 키워드 검색
+     */
+    @GetMapping("keywork/{keyword}")
+    public ResponseEntity<ResponseDto> searchKeyword(@PathVariable String keyword) {
+        return ResponseEntity.ok(ResponseDto.builder()
+                .message("")
+                .data("")
+                .build());
+
     }
 }

af416451c0877b96336d097d0edf03c394fd1950
공진호 <wlsgh7608@naver.com>
2023-05-14 02:27:25 +0900

    ⚡️ feat: Favorite Return Type 변경

--- server/favorite-service/src/main/java/kr/stockey/favoriteservice/api/FavoriteController.java
+++ server/favorite-service/src/main/java/kr/stockey/favoriteservice/api/FavoriteController.java
@@ -1,7 +1,6 @@
 package kr.stockey.favoriteservice.api;
 
 import kr.stockey.favoriteservice.dto.core.FavoriteDto;
-import kr.stockey.favoriteservice.dto.core.ResponseDto;
 import kr.stockey.favoriteservice.service.FavoriteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,67 +21,65 @@ public class FavoriteController {
     /* --------------  다른 서비스에서 호출하는 메소드 [start] ----------------  */
 
     @GetMapping("/client/industry/my")
-    public ResponseEntity<ResponseDto> getMyFavoriteIndustry() {
+    public ResponseEntity<List<FavoriteDto>> getMyFavoriteIndustry() {
         Long memberId = getMemberId();
         List<FavoriteDto> favoriteDtoList = favoriteService.findByIndustry(memberId);
-        return new ResponseEntity<>(new ResponseDto("OK", favoriteDtoList), HttpStatus.OK);
+        return new ResponseEntity<>(favoriteDtoList, HttpStatus.OK);
     }
 
     @GetMapping("/client/stock/my")
-    public ResponseEntity<ResponseDto> getMyFavoriteStock() {
+    public ResponseEntity<List<FavoriteDto>> getMyFavoriteStock() {
         Long memberId = getMemberId();
         List<FavoriteDto> favoriteDtoList = favoriteService.findByStock(memberId);
-        return new ResponseEntity<>(new ResponseDto("OK", favoriteDtoList), HttpStatus.OK);
+        return new ResponseEntity<>(favoriteDtoList, HttpStatus.OK);
     }
 
     @GetMapping("/client/keyword/my")
-    public ResponseEntity<ResponseDto> getMyFavoriteKeyword() {
+    public ResponseEntity<List<FavoriteDto>> getMyFavoriteKeyword() {
         Long memberId = getMemberId();
         List<FavoriteDto> favoriteDtoList = favoriteService.findByKeyword(memberId);
-        return new ResponseEntity<>(new ResponseDto("OK", favoriteDtoList), HttpStatus.OK);
+        return new ResponseEntity<>(favoriteDtoList, HttpStatus.OK);
     }
 
     @GetMapping("/client/industry/check/{industryId}")
-    public ResponseEntity<ResponseDto> checkFavoriteIndustry(@PathVariable Long industryId) {
+    public ResponseEntity<Boolean> checkFavoriteIndustry(@PathVariable Long industryId) {
         Long memberId = getMemberId();
         boolean result = favoriteService.existsByMemberAndIndustry(industryId, memberId);
-        return new ResponseEntity<>(new ResponseDto("OK", result), HttpStatus.OK);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("/client/stock/check/{stockId}")
-    public ResponseEntity<ResponseDto> checkFavoriteStock(@PathVariable Long stockId) {
+    public ResponseEntity<Boolean> checkFavoriteStock(@PathVariable Long stockId) {
         Long memberId = getMemberId();
         boolean result = favoriteService.existsByMemberAndStock(stockId, memberId);
-        return new ResponseEntity<>(new ResponseDto("OK", result), HttpStatus.OK);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("keyword/check/{keywordId}")
-    public ResponseEntity<ResponseDto> checkFavoriteKeyword(@PathVariable Long keywordId) {
+    public ResponseEntity<Boolean> checkFavoriteKeyword(@PathVariable Long keywordId) {
         Long memberId = getMemberId();
         boolean result = favoriteService.existsByMemberAndKeyword(keywordId, memberId);
-        return new ResponseEntity<>(new ResponseDto("OK", result), HttpStatus.OK);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @PostMapping("/client/industry/{industryId}")
-    public ResponseEntity<ResponseDto> createFavoriteIndustry(@PathVariable Long industryId) {
+    public ResponseEntity<Void> createFavoriteIndustry(@PathVariable Long industryId) {
         Long memberId = getMemberId();
         favoriteService.createFavoriteIndustry(industryId, memberId);
-        return new ResponseEntity<>(new ResponseDto("CREATED", null), HttpStatus.CREATED);
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
     }
 
     @PostMapping("/client/stock/{stockId}")
-    public ResponseEntity<ResponseDto> createFavoriteStock(@PathVariable Long stockId) {
+    public ResponseEntity<Void> createFavoriteStock(@PathVariable Long stockId) {
         Long memberId = getMemberId();
         favoriteService.createFavoriteStock(stockId, memberId);
-        return new ResponseEntity<>(new ResponseDto("CREATED", null), HttpStatus.CREATED);
-    }
+        return new ResponseEntity<>(null, HttpStatus.CREATED);    }
 
     @PostMapping("/client/keyword/{keywordId}")
-    public ResponseEntity<ResponseDto> createFavoriteKeyword(@PathVariable Long keywordId) {
+    public ResponseEntity<Void> createFavoriteKeyword(@PathVariable Long keywordId) {
         Long memberId = getMemberId();
         favoriteService.createFavoriteKeyword(keywordId, memberId);
-        return new ResponseEntity<>(new ResponseDto("CREATED", null), HttpStatus.CREATED);
-    }
+        return new ResponseEntity<>(null, HttpStatus.CREATED);    }
 
     @DeleteMapping("/client/keyword/{industryId}")
     public ResponseEntity<Void> deleteFavoriteIndustry(@PathVariable Long industryId) {
@@ -90,6 +87,7 @@ public class FavoriteController {
         favoriteService.deleteFavoriteIndustry(industryId, memberId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
     @DeleteMapping("/client/keyword/{stockId}")
     public ResponseEntity<Void> deleteFavoriteStock(@PathVariable Long stockId) {
         Long memberId = getMemberId();
@@ -104,7 +102,6 @@ public class FavoriteController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
     /* --------------  다른 서비스에서 호출하는 메소드 [end]  ----------------  */
-
 
 
     private Long getMemberId() {

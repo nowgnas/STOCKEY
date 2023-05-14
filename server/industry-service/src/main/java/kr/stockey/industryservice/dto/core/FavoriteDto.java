5f35fd12bd0bf04fca7fed3eabe3abf5ed8c8e32
공진호 <wlsgh7608@naver.com>
2023-05-14 11:11:33 +0900

    ✨ feat: Favorite dto 생성

---
+++ server/industry-service/src/main/java/kr/stockey/industryservice/dto/core/FavoriteDto.java
@@ -0,0 +1,15 @@
+package kr.stockey.industryservice.dto.core;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class FavoriteDto {
+    private Long id;
+    private Long memberId;
+    private Long stockId;
+    private Long industryId;
+    private Long keywordId;
+
+}

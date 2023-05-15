d98761c1bfde4abddb45270aceb9062c04e218ba
공진호 <wlsgh7608@naver.com>
2023-05-16 03:13:19 +0900

    ♻️ refact: client 메소드 변경

--- server/keyword-service/src/main/java/kr/stockey/keywordservice/client/FavoriteClient.java
+++ server/keyword-service/src/main/java/kr/stockey/keywordservice/client/FavoriteClient.java
@@ -1,25 +1,27 @@
 package kr.stockey.keywordservice.client;
 
-import kr.stockey.keywordservice.dto.core.ResponseDto;
+import kr.stockey.keywordservice.dto.core.FavoriteDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @FeignClient(name = "favorite-service")
 public interface FavoriteClient {
 
-    @GetMapping("/favorite/keyword/my")
-    ResponseDto getMyFavoriteKeyword();
+    @GetMapping("/favorite/client/keyword/my")
+    List<FavoriteDto> getMyFavoriteKeyword();
 
-    @GetMapping("/favorite/keyword/check/{keywordId}")
-    ResponseDto checkFavoriteKeyword(@PathVariable Long keywordId);
+    @GetMapping("/favorite/client/keyword/check/{keywordId}")
+    Boolean checkFavoriteKeyword(@PathVariable Long keywordId);
 
-    @PostMapping("/favorite/keyword/{keywordId}")
-    ResponseDto createFavoriteKeyword(@PathVariable Long keywordId);
+    @PostMapping("/favorite/client/keyword/{keywordId}")
+    Void createFavoriteKeyword(@PathVariable Long keywordId);
 
-    @DeleteMapping("/favorite/keyword/{keywordId}")
+    @DeleteMapping("/favorite/client/keyword/{keywordId}")
     Void deleteFavorite(@PathVariable Long keywordId);
 
 }

c41394c86fa87ef5c6059bc79c3cd456e9e6fa16
junmo <devjunmo@gmail.com>
2023-05-15 17:59:24 +0900

    ✅ feat: jpql 메소드 구현 및 날짜 얻어내는 테스트 코드 구현
    Former-commit-id: 6b95752756b5b8b410cef322e27fa1b89e70bca2

--- server/investment-service/src/test/java/kr/stockey/investmentservice/JustTest.java
+++ server/investment-service/src/test/java/kr/stockey/investmentservice/JustTest.java
@@ -4,9 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class JustTest {
@@ -49,7 +55,29 @@ public class JustTest {
 
         List<PersonDTO> personDTOs = List.of(new PersonDTO(name, age));
 
-        Assertions.assertEquals(name, personDTOs.get(0).getName());
-        Assertions.assertEquals(age, personDTOs.get(0).getAge());
+        assertEquals(name, personDTOs.get(0).getName());
+        assertEquals(age, personDTOs.get(0).getAge());
     }
+
+    @Test
+    public void 특정날짜기준_그_주의_월요일_일요일_가져오기() {
+        LocalDate today = LocalDate.of(2023, 5, 14); // 예시로 2023년 5월 14일을 사용합니다.
+
+        LocalDate sunday = today.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+        LocalDate monday = sunday.minusDays(6);
+
+        List<LocalDate> expectedDates = Arrays.asList(monday, sunday);
+
+        List<LocalDate> actualDates = getWeekDates(today);
+
+        assertEquals(expectedDates, actualDates);
+    }
+
+    private List<LocalDate> getWeekDates(LocalDate date) {
+        LocalDate sunday = date.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+        LocalDate monday = sunday.minusDays(6);
+
+        return Arrays.asList(monday, sunday);
+    }
+
 }

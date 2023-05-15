f5cdc71cbf605e4d99cb1207d6d014a693ae7388
공진호 <wlsgh7608@naver.com>
2023-05-15 17:41:37 +0900

    🔧 amend: 뉴스 url 변경

--- server/news-service/src/main/java/kr/stockey/newsservice/api/NewsController.java
+++ server/news-service/src/main/java/kr/stockey/newsservice/api/NewsController.java
@@ -1,12 +1,14 @@
 package kr.stockey.newsservice.api;
 
 import kr.stockey.newsservice.api.request.NewsCountRequest;
-import kr.stockey.newsservice.dto.core.ResponseDto;
 import kr.stockey.newsservice.service.NewsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -16,11 +18,16 @@ import javax.validation.Valid;
 public class NewsController {
     private final NewsService newsService;
 
-    @PostMapping("/domain/count")
-    public ResponseEntity<ResponseDto> getNewsCountByDomain(@RequestBody @Valid NewsCountRequest request) {
+    /* --------------  다른 서비스에서 호출하는 메소드 [start] ----------------  */
+    @PostMapping("/client/domain/count")
+    public ResponseEntity<Long> getNewsCountByDomain(@RequestBody @Valid NewsCountRequest request) {
         Long count = newsService.getTotalNewsCount(request);
-        return new ResponseEntity<>(new ResponseDto("OK", count), HttpStatus.OK);
+        return new ResponseEntity<>(count, HttpStatus.OK);
     }
+
+    /* --------------  다른 서비스에서 호출하는 메소드 [end]  ----------------  */
+
+
 
 
 }

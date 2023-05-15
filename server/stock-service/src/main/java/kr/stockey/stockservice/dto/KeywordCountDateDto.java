7c0822a695d74f2fb3dfac1bf24aec40f8ca1b67
공진호 <wlsgh7608@naver.com>
2023-05-16 02:02:20 +0900

    ⚡️ feat: 상관분석 구현

---
+++ server/stock-service/src/main/java/kr/stockey/stockservice/dto/KeywordCountDateDto.java
@@ -0,0 +1,12 @@
+package kr.stockey.stockservice.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class KeywordCountDateDto {
+    private LocalDate statisticDate;
+    private Long count;
+
+}

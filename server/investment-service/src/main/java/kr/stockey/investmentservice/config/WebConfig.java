97de2f2a766204876b934d29dfd09fbeffcfd61e
junmo <devjunmo@gmail.com>
2023-05-16 14:03:38 +0900

    🚑️ fix: webconfig 수정
    Former-commit-id: 7f60da4c100ab76e6b301bdb3f910059cc6f9af5

--- server/investment-service/src/main/java/kr/stockey/investmentservice/config/WebConfig.java
+++ server/investment-service/src/main/java/kr/stockey/investmentservice/config/WebConfig.java
@@ -1,9 +1,11 @@
 package kr.stockey.investmentservice.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {

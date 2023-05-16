e1b91d2ad3e52f18bb3c93b5447ccfdb4b44e729
공진호 <wlsgh7608@naver.com>
2023-05-17 02:11:40 +0900

    ⚡️ feat: 상관관계 분석

--- server/stock-service/src/main/java/kr/stockey/stockservice/repository/StockRepository.java
+++ server/stock-service/src/main/java/kr/stockey/stockservice/repository/StockRepository.java
@@ -24,7 +24,7 @@ public interface StockRepository extends JpaRepository<Stock,Long> {
             "WHERE \n" +
             "ds.stock_id in (select stock_id from stock t where t.industry_id=:industryId)) AS temp \n" +
             "GROUP BY temp.stock_date;",nativeQuery = true)
-    List<IndustrySumDto> getMarketList(Long industryId);
+    List<IndustrySumDto> getMarketList(@Param("industryId") Long industryId);
 
     Optional<Stock> findById(Long id);
 

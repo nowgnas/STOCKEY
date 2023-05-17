3b1fa1f033224bb856c26f65cf9f087a1c36d06f
이상원 <marshmello.sw@gmail.com>
2023-05-17 09:43:55 +0900

    ➕ add dependency
    Former-commit-id: 0aa31a6a67753cc8dd6c329da1bde96b402ac8eb

--- server/investment-service/src/main/java/kr/stockey/investmentservice/InvestmentServiceApplication.java
+++ server/investment-service/src/main/java/kr/stockey/investmentservice/InvestmentServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
+@EnableEurekaClient
 public class InvestmentServiceApplication {
 
     public static void main(String[] args) {

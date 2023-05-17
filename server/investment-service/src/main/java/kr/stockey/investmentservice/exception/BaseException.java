3b6f520c638ef6f0b5652464997d192dacc25c85
양준모 <devjunmo@gmail.com>
2023-05-17 13:48:03 +0900

    Revert "Merge branch 'dev-be/investment-jun-dev' into 'main'"
    This reverts merge request !90
    
    Former-commit-id: a35367d21e4dce6e285581f495afa11100bb8a35

--- server/investment-service/src/main/java/kr/stockey/investmentservice/exception/BaseException.java
+++
@@ -1,6 +0,0 @@
-package kr.stockey.investmentservice.exception;
-
-public abstract class BaseException extends RuntimeException {
-    public abstract BaseExceptionType getExceptionType();
-}
-
--- server/investment-service/src/main/java/kr/stockey/investmentservice/exception/BaseExceptionType.java
+++
@@ -1,9 +0,0 @@
-package kr.stockey.investmentservice.exception;
-
-import org.springframework.http.HttpStatus;
-
-public interface BaseExceptionType {
-    HttpStatus getHttpStatus();
-    String getErrorMessage();
-}
-

1e1db0733a2992e3c7556f2c8679b77de101721a
공진호 <wlsgh7608@naver.com>
2023-05-17 21:19:44 +0900

    ⚡️ feat: StockRequest 어노테이션 추가

--- server/stock-service/src/main/java/kr/stockey/stockservice/api/request/GetLikeStockRankRequest.java
+++ server/stock-service/src/main/java/kr/stockey/stockservice/api/request/GetLikeStockRankRequest.java
@@ -1,10 +1,16 @@
 package kr.stockey.stockservice.api.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class GetLikeStockRankRequest {
     List<Long> stockList;
     Long stockId;

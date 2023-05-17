f4952f8e0a710ec9837ce6e1021b79c51f53c828
nowgnas <dev.nowgnas@gmail.com>
2023-05-17 13:23:21 +0900

    :heavy_plus_sign: Add spring cloud dependency
    Former-commit-id: eee0fdde457d8c8f6ee8cc5eda1ff01155a152f4

--- server/investment-service/src/main/java/kr/stockey/investmentservice/InvestmentServiceApplication.java
+++ server/investment-service/src/main/java/kr/stockey/investmentservice/InvestmentServiceApplication.java
@@ -2,9 +2,9 @@ package kr.stockey.investmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableScheduling
